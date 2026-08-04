091ccb497220b094cb79f72536803e02f038f720
cfspec <cepfikri16@gmail.com>
2026-08-04 13:25:16 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
-import { app } from "../server";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Pastikan Anda punya file App.tsx atau App.ts di root
+import './index.css';     // Jika Anda menggunakan file CSS utama
 
-export default app;
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
